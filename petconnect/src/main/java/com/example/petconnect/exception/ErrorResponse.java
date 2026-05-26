d204c2e4f0add191186d029b2f82e2a5d1b9dcde
polyana <polinhasantos74@gmail.com>
2026-05-25 21:11:28 -0300

    feat: Atualizações tratamento de erros

--- petconnect/src/main/java/com/example/petconnect/exception/ErrorResponse.java
+++ petconnect/src/main/java/com/example/petconnect/exception/ErrorResponse.java
@@ -1,45 +1,18 @@
 package com.example.petconnect.exception;
 
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class ErrorResponse {
 
     private LocalDateTime timestamp;
-    private int status;
-    private String error;
-    private String message;
-    private String path;
 
-    public ErrorResponse(
-            LocalDateTime timestamp,
-            int status,
-            String error,
-            String message,
-            String path) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.path = path;
-    }
+    private Integer status;
 
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
+    private String mensagem;
 
-    public int getStatus() {
-        return status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getPath() {
-        return path;
-    }
 }
