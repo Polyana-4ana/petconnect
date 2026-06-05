bfa054c4efaadea42e6becb17ddfc03177d486e7
Polyana Sanhudo <polinhasantos74@gmail.com>
2026-06-05 19:09:49 -0300

    feat: Refinando Tratamento de erros

--- petconnect/src/main/java/com/example/petconnect/exception/GlobalExceptionHandler.java
+++ petconnect/src/main/java/com/example/petconnect/exception/GlobalExceptionHandler.java
@@ -8,20 +8,72 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(
-            IllegalArgumentException.class)
-
-    @ResponseStatus(
-            HttpStatus.BAD_REQUEST)
-
-    public ErrorResponse tratarErro(
-            IllegalArgumentException ex){
+    @ExceptionHandler(PetNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse tratarPetNaoEncontrado(
+            PetNaoEncontradoException ex) {
 
         return new ErrorResponse(
                 LocalDateTime.now(),
-                400,
+                404,
+                "NOT_FOUND",
                 ex.getMessage()
         );
     }
 
+    @ExceptionHandler(
+            AdotanteNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse tratarAdotanteNaoEncontrado(
+            AdotanteNaoEncontradoException ex) {
+
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                404,
+                "NOT_FOUND",
+                ex.getMessage()
+        );
+    }
+
+    @ExceptionHandler(
+            AdocaoNaoEncontradaException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse tratarAdocaoNaoEncontrada(
+            AdocaoNaoEncontradaException ex) {
+
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                404,
+                "NOT_FOUND",
+                ex.getMessage()
+        );
+    }
+
+    @ExceptionHandler(
+            IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse tratarRegraNegocio(
+            IllegalArgumentException ex) {
+
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                400,
+                "BAD_REQUEST",
+                ex.getMessage()
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(
+            HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse tratarErroGenerico(
+            Exception ex) {
+
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                500,
+                "INTERNAL_SERVER_ERROR",
+                ex.getMessage()
+        );
+    }
 }
