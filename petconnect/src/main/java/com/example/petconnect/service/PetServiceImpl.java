fbb8d2a153a7ed571daeb44dc15bc357cbd530c8
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2026-06-14 18:04:04 -0300

    fix: corrige a importacao

--- petconnect/src/main/java/com/example/petconnect/service/PetServiceImpl.java
+++ petconnect/src/main/java/com/example/petconnect/service/PetServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.data.jpa.domain.Specification;
-import com.example.petconnect.repository.specification.PetSpecification;
+import com.example.petconnect.repository.PetSpecification;
 import java.util.List;
 import java.util.stream.Collectors;
 
